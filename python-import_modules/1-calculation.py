385c037d633faae7f6e3937ac02f0355c8ab1071
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-08 15:09:31 +0200

    adding .format()

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print(f"{a} + {b} = {add(a,b)}")
-    print(f"{a} + {b} = {sub(a,b)}")
-    print(f"{a} + {b} = {mul(a,b)}")
-    print(f"{a} + {b} = {div(a,b)}")
+    print(f"{a} + {b} = {add(a,b)}".format())
+    print(f"{a} + {b} = {sub(a,b)}".format())
+    print(f"{a} + {b} = {mul(a,b)}".format())
+    print(f"{a} + {b} = {div(a,b)}".format())

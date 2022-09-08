c78b2ae3cb1ff6ae66f56520c2f69374dc93dabc
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-08 12:54:03 +0200

    adding all functions for the project

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+for i in range(26):
+    if i % 2 == 1:
+        i += 32
+    print(chr(122 - i), end='')
+    if i % 2 == 1:
+        i -= 32
---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from operator import truediv
+
+
+def islower(c):
+    if ord(c) >= 97 and ord(c) < 123:
+        return (True)
+    return (False)

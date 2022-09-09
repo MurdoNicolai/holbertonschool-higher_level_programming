9e862c14e3bd1ee52133c0115e734f54942221bb
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-09 17:54:51 +0200

    3.0.1 bugfix first arg summed

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,7 +4,11 @@ from sys import argv
 
 if __name__ == "__main__":
     num = 0
+    check = 0
     import sys
     for args in sys.argv:
-        num += int(args)
+        if check == 0:
+            check = 1
+        else:
+            num += int(args)
     print(f"{num}".format())

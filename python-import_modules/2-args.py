b87096d0ae8e2206fe627846cbd12603d2d12637
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-09 17:48:54 +0200

    changing print for single args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,14 @@ from sys import argv
 if __name__ == "__main__":
     i = 0
     import sys
-    print(f"{len(argv) - 1} arguments:".format())
+    print(f"{len(argv) - 1} argument".format(), end='')
+    if (len(argv) - 1) == 0:
+        print(".")
+    elif (len(argv) - 1) == 1:
+        print(":")
+    else:
+        print("s:")
+
 
     for args in sys.argv:
         if i != 0:

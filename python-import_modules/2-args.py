4fde7bb8630b098e6257b704cb5585c9f1785758
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-09 17:50:27 +0200

    2.0 final check

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,13 +7,11 @@ if __name__ == "__main__":
     import sys
     print(f"{len(argv) - 1} argument".format(), end='')
     if (len(argv) - 1) == 0:
-        print(".")
+        print("s.")
     elif (len(argv) - 1) == 1:
         print(":")
     else:
         print("s:")
-
-
     for args in sys.argv:
         if i != 0:
             print(f"{i}: {args}".format())

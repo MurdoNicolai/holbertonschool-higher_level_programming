cb39c21926b09b7046b81d390bd0b2fe8573b30b
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-18 15:42:06 +0200

    adding error

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for elem in my_list:
         if count < x:
             try:
-                print(f"{elem}".format(), end = '')
-            except:
+                print(f"{elem}".format(), end='')
+            except ValueError:
                 print("error")
             count += 1
     print()

94687aefb40f116ffdf9866997ba77027ec7e43e
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-19 10:10:17 +0200

    2.0 checking if length is ok

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    creating the square class
+"""
+
+
+class Square:
+    """
+    Defines a géometrical square.
+
+    Args:
+        __size (int): length of side
+
+    """
+    def __init__(self, size=0):
+        """
+        Defines _size
+
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size

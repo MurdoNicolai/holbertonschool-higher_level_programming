c749bcbc6a9b8c1a1255ab65fd4cda3de5e83c59
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-19 10:51:03 +0200

    5.0 adding print square

---
+++ python-classes/5-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+    creating the square class
+"""
+
+
+class Square:
+    """
+    Defines a géometrical square.
+
+    Args:
+        __size (int): length of side
+
+    """
+    def __init__(self, size=0):
+        """
+        Defines _size
+
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Return area of the square
+
+        """
+        return self.__size ** 2
+
+    def set_size(self, value):
+        """
+        Set new size of the square
+
+        args:
+            value (int): value of new size must be positive
+
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def get_size(self):
+        """
+        Return size of the square
+
+        """
+        return self.__size
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print()
+
+    size = property(get_size, set_size)

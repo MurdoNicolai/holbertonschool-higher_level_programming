ffc8db534062d6b55be7779ba3a2d224625de5c7
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-19 10:14:42 +0200

    3.0 adding return area option

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    creating the square class
+"""
+
+
+class Square:
+    """
+    Defines a géometrical square.
+
+    Args:
+        __size (int): length of side
+
+    """
+    def __init__(self, size=0):
+        """
+        Defines _size
+
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Return area of the square
+
+        """
+        return self.__size ** 2

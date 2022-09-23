709879f11597fdc3f301cec32227257e8dcb12bc
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-23 09:03:59 +0200

    adding matrix devision code

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+""" another testing ground but with slightly more complex function:
+    matirx devision """
+
+def matrix_divided(matrix, div):
+    """ devides all numbers in a matrix by a number:
+        matrix: matrix to devide
+        div: number to devide matrix by"""
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if (type(matrix) is not list or (len(matrix) == 0) or
+        type(matrix[0]) is not list or (len(matrix[0]) == 0)):
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for c in row:
+            if type(c) is not int and type(c) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    return [[round(item / div, 2) for item in row] for row in matrix]

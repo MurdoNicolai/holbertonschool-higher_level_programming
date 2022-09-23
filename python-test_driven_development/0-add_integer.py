df23a13923b2e26adc5299c30b0ad3b64b139062
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-23 08:41:08 +0200

    deleting useless code'

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,8 +6,6 @@ def add_integer(a, b=98):
     """adds together to numbers (float or int):
         a: first number
         b: second number
-        >>> add_integer(1, 2)
-        3
         """
     if (not isinstance(a,int)) and (not isinstance(a, float)):
         raise TypeError("a must be an integer")

33873eb79c14f74a1fb487000e26141b758daf77
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-23 11:09:59 +0200

    adding correct tests to testfile

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,13 +8,28 @@ toTest = importlib.import_module('6-max_integer')
 
 class Testmaxint(unittest.TestCase):
 
-    def test_Normalcase(self):
+    def test_maxAtEnd(self):
+        self.assertEqual(toTest.max_integer([5, 3, 1, 0, 7]), 7)
+
+    def test_maxAtStart(self):
+        self.assertEqual(toTest.max_integer([5, 3, 1, 0, 4]), 5)
+
+    def test_maxInMiddle(self):
+        self.assertEqual(toTest.max_integer([5, 3, 10, 0, 4]), 10)
+
+    def test_1negative(self):
         self.assertEqual(toTest.max_integer([5, 3, 1, 0, -4]), 5)
+
+    def test_allnegative(self):
+        self.assertEqual(toTest.max_integer([-5, -3, -1, -4]), -1)
+
+    def test_1elem(self):
+        self.assertEqual(toTest.max_integer([2]), 2)
+
+
+    def test_emptyu(self):
         self.assertEqual(toTest.max_integer([]), None)
-        with self.assertRaises(TypeError):
-            toTest.max_integer("word")
-        with self.assertRaises(TypeError):
-            toTest.max_integer(["ball", "word"])
+
 
 
 if __name__ == '__main__':

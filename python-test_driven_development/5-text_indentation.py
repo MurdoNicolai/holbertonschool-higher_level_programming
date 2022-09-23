00c647f0b53772bc15dadd2e8ed5081e20ca49bd
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-23 09:54:15 +0200

    adding print text + test file

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+""" test on modifying text file function """
+
+def text_indentation(text):
+    """ prints a string by adding 2 new lines after every . or ? or :
+        text: string to print"""
+    text_update = str(text)
+    after_new_line = False
+    for c in text_update:
+        if after_new_line:
+            if c == " ":
+                continue
+            after_new_line = False
+        if c == '.' or c == '?' or c == ':':
+            print(c)
+            print("")
+            after_new_line = True
+        else:
+            print(c, end="")

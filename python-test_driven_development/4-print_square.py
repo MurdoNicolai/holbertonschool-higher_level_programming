f80e3bb5709672c1cd02ed5b13b03e1f59551b54
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-23 09:43:32 +0200

    adding print square + test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" now we try to print some graphics and test that """
+
+
+def print_square(size):
+    """ print a square of # of given size:
+        size: size of square"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for r in range(0, size):
+        for c in range(0, size):
+            if c != (size - 1):
+                print("#", end="")
+            else:
+                print("#")

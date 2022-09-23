98d640c35e317250b75d89d8134b947d4a6836cf
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-23 08:47:58 +0200

    reorganising and cleaning up

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,15 @@
-"""contains a simple addition function to practice making test files
-        """
-
 #!/usr/bin/python3
+"""contains a simple addition function to practice making test files"""
+
+
 def add_integer(a, b=98):
     """adds together to numbers (float or int):
         a: first number
         b: second number
         """
-    if (not isinstance(a,int)) and (not isinstance(a, float)):
+    if (not isinstance(a, int)) and (not isinstance(a, float)):
         raise TypeError("a must be an integer")
-    elif (not isinstance(b,int)) and (not isinstance(b, float)):
+    elif (not isinstance(b, int)) and (not isinstance(b, float)):
         raise TypeError("b must be an integer")
     else:
-        print(f"{int(a) + int(b)}")
+        return int(a) + int(b)

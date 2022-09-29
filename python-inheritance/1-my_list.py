a8c08d608e2ab5922c5432174b1174240a7e0834
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-29 21:12:26 +0200

    1 adding class that inherits frome list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creating MyList that inherits frome list"""
+
+
+class MyList(list):
+    """inherits the list class but we add sorted print function:
+        print_sorted (func): prints the lists but sorted without changes
+    """
+    def print_sorted(self):
+            print(sorted(self))

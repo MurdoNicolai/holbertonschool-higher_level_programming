91d2790ee461f3e3626f7c73c65594bccf40f70c
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-29 19:53:02 +0200

    101.3 pleasing pycodestyle

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -3,15 +3,21 @@
 Creating a class that can only have 1 specifique attribute:
     LockedClass
 """
+
+
 class LockedClass():
     """
         Prevent user frome creating an instance atribute other than first_name
     """
+
     def __getattribute__(self, key):
         if key != "first_name":
-            raise AttributeError(f"'LockedClass' object has no attribute '{key}'")
+            raise AttributeError(f"'LockedClass' object"
+                                 f" has no attribute '{key}'")
+
     def __setattr__(self, key, value):
         if key != "first_name":
-            raise AttributeError(f"'LockedClass' object has no attribute '{key}'")
+            raise AttributeError(f"'LockedClass' object"
+                                 f" has no attribute '{key}'")
         object.__setattr__(self, key, value)
     pass

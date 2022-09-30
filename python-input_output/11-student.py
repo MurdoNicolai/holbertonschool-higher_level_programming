fb72b92eacf3166395832303be42a6a1d948d06c
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 20:20:04 +0200

    11 adding load attribute frome dictionary

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+""" has class students"""
+
+
+class Student():
+    """student:
+    first_name (string)
+    last_name (string)
+    age (int)
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            newdict = {}
+            for key in attrs:
+                if self.__dict__.get(key):
+                    newdict[key] = self.__dict__.get(key)
+            return newdict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for value in json:
+            setattr(self, value, json[value])

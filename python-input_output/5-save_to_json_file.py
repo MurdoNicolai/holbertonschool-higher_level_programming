6806d87ad295026d352b4e1cdabdda0ff5af2d7e
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 18:10:08 +0200

    adding read and write frome json text file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""has function that writes to textfile the JSON representation of an object"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """returns that writes to textfile theJSON representation of:
+        my_obj (string)
+        filename (sting): file to write to
+    """
+    with open(filename, "w") as filename:
+        json.dump(my_obj, filename)
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""function that reads from textfile the JSON representation of an object"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """returns that reads frome textfile the JSON representation of:
+        my_obj (string) and creates it
+        filename (sting): file to read to
+    """
+    with open(filename, "r") as filename:
+        json.load(my_obj, filename)

347f3cbe71aa2b72dc5db1e6c4480ad01487ebd2
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 12:54:10 +0200

    7 adding test on value

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" countains some classes for basic geometry:
+    BaseGeometry: define the basic rules"""
+
+
+class BaseGeometry:
+    """defines some basic geometry rules to be used in subclasses"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if value is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")

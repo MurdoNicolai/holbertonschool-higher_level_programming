dacb479f5ec490d631331ef60eb1183e48061f8c
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 14:45:55 +0200

    12 adding rebbel int

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" creating a rebel my int class"""
+
+
+class MyInt(int):
+    """an int class that inverts == and !="""
+
+    def __eq__(self, __x):
+        return super().__ne__(__x)
+
+    def __ne__(self, __x):
+        return super().__eq__(__x)

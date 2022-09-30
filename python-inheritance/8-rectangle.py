ff72f05f79194ca3582b90792f4cb0027ab11c8b
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 13:26:12 +0200

    adding rectangle subclass

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+""" countains some classes for basic geometry:
+    BaseGeometry: define the basic rules
+    Rectangle(BaseGeometry): defines a rectangle"""
+
+
+class BaseGeometry:
+    """defines some basic geometry rules to be used in subclasses"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not type(value) is int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """defines some basic geometry rules for rectangles:
+    height (int): hight of rectangle
+    width (int): width of rectangle
+    """
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(BaseGeometry, self.__width, width)
+        BaseGeometry.integer_validator(BaseGeometry, self.__height, height)
+        self.__width = width
+        self.__height = height

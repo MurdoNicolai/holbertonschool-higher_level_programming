add50d82e605804b2311a7fbed035241d672f727
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 15:53:46 +0200

    adding test if exist

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""adding test if attribute can be added"""
+
+
+def add_attribute(object, key, value):
+    """basacly we just see if object has __dict__ then add it if it has"""
+    if not hasattr(object, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(object, key, value)

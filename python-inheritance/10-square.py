679fdbd06e52e9ae73a4bbe128bfa3b4a2d3b7ee
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 14:07:09 +0200

    10 adding square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" countains some classes for basic geometry:
+    Square(Rectangle): defines a square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """defines some basic geometry rules for rectangles:
+    size (int): hight and width of square
+    """
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = self._Rectangle__width

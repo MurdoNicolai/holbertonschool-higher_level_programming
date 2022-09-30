0c8ac8184cf922a4a93ca10661cabe3a432ef789
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 17:45:50 +0200

    minor bugfixes on first 3 functions

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """reads and prints inputed file
         filename (string): file to read"""
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end='')
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,3 +8,4 @@ def write_file(filename="", text=""):
     text (string): what to write in file"""
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(text)
+        return len(text)

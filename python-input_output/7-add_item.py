836c6212ea58b321eae77511a73d050ba4f4c405
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 18:50:21 +0200

    adding 7: appendin to a list in jason file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""adds all arguments to a python list then saves them to
+    a file called add_item.json"""
+import json
+import sys
+load_frome_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+try:
+    newlist = load_frome_json_file("add_item.json")
+except FileNotFoundError:
+    newlist = list()
+newlist.extend(sys.argv[1:])
+save_to_json_file(newlist, "add_item.json")

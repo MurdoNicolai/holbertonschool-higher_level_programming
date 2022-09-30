09588b400e22742034d1885a204db1ec0cc160be
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 17:52:50 +0200

    adding text to json converter

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""has function that returns the JSON representation of an object """
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of:
+        my_obj (string)
+    """
+    return json.dumps(my_obj)

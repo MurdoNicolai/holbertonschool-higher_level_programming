d8b1f0abc04f561489898b9fd0fe08c4c1727185
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 13:33:05 +0200

    8 importing 7 instead of copy paste

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 """ countains some classes for basic geometry:
-    BaseGeometry: define the basic rules
     Rectangle(BaseGeometry): defines a rectangle"""
 
 
-class BaseGeometry:
-    """defines some basic geometry rules to be used in subclasses"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not type(value) is int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

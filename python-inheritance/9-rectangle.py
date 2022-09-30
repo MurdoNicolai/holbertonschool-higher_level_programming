a6f4bf3b8adc7bef207a6231c237832516934301
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 13:42:11 +0200

    9 adding return value for area

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" countains some classes for basic geometry:
+    Rectangle(BaseGeometry): defines a rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """defines some basic geometry rules for rectangles:
+    height (int): hight of rectangle
+    width (int): width of rectangle
+    """
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(BaseGeometry, "width", width)
+        BaseGeometry.integer_validator(BaseGeometry, "height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width*self.__height
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")

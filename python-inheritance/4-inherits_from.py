8c7f49c0f1ca2ff677d500233baaaef1a991dac9
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 12:43:48 +0200

    4 adding function that test inderect inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""has the is_same_class function which test if obj is a certain class
+    or inherits forme that class directly or not"""
+
+
+def def inherits_from(obj, a_class):
+    """test if object is inheritant of a certain classdirectly or not:
+        obj: object to test
+        a_class: the class it should inherit frome or be
+        return: true or false"""
+    return (isinstance(obj, a_class))

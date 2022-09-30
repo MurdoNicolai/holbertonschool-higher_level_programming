71ef91e0bee72322b35d5a05cd64d63518f09b28
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 19:53:20 +0200

    10 trying to add adaptable dictionary return on student class

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" has class students"""
+
+
+class Student():
+    """student:
+    first_name (string)
+    last_name (string)
+    age (int)
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            newdict = {}
+            for key in list:
+                newdict.update((key, self.__dict__.get(key)))
+        else:
+            return self.__dict__

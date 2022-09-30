8eb93fc5abbf8432b19b86e82e97fbd42ec5b45d
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 18:57:08 +0200

    adding encoder

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""has function that returns description for JSON serialization of an object"""
+import json
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    for JSON serialization of an object:
+        obj (object): object to describe
+    """
+    return json.encode(obj)

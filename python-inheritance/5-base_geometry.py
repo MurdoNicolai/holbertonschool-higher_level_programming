6114e572bb9b53c674829ca13c1f3dfbd2e184c4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 12:49:06 +0200

    5 adding geometry class

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" countains some classes for basic geometry:
+    BaseGeometry: define the basic rules"""
+
+
+class BaseGeometry:
+    """defines some basic geometry rules to be used in subclasses"""
+    pass

bb5eeef6e250e9ffe102341ff6496f8f8cc18600
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-09-30 19:33:30 +0200

    9 adding students class

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" has class students"""
+
+
+class Student():
+    """student:
+    first_name (string)
+    last_name (string)
+    age (int)
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__

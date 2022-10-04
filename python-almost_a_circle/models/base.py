5d7f55058d9ae21d84baa0b0796d2462c9655f35
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-10-04 15:49:19 +0200

    adding check on frome lson string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -41,4 +41,6 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation: json_string"""
+        if json_string is None or json_string == list():
+            return list()
         return json.loads(json_string)

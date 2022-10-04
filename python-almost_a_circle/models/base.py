03b3fdcb5e49dd5a39f08b2bcec8d5b6f2a4fd43
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-10-04 10:38:15 +0200

    minor bbugfix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """ contains the Base class """
 
-
 class Base:
     """ Serves as Base for the other classes of the project:
         id (int): is diffrent for each object."""
 
+    __nb_objects = 0
+
     def __init__(self, id=None):
-        __nb_objects = 0
-        if id:
-            id = id
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
         else:
-            __nb_objects += 1
-            id = __nb_objects
+            self.id = id
+
 
 

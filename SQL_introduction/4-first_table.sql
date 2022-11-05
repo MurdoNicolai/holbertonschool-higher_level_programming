8a142e4e50a43aaadffcdbb00b13a40e88dddb63
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-05 08:19:03 +0100

    adding epmty exos and show create  table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Write a script that creates a table called first_table in the current database in your MySQL server.
-CREATE TABLE first_table(
-    id INT,
-    name VARCHAR(256)
-);
+CREATE TABLE `first_table` (
+	  `id` int DEFAULT NULL,
+	  `name` varchar(256) DEFAULT NULL
+	  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- Write a script that prints the full description of the table
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "first_table";
+SHOW CREATE TABLE first_table;

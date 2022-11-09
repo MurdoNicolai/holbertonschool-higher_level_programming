78c0db29870ce4513c1483928f5d4ce1f66eb5ca
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-09 06:09:11 +0100

    changing 2 to only grant select

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- creates a database and a user that only has privelege to that database
+-- creates a database and a user that only has select privelege to that database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE user IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

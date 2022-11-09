1ce6722046fe68e8acd0e94ee5feb269dd371fa4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-09 06:34:16 +0100

    adding create table script with id = 1

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- creates unique_id same as table 4 but id is unique
-CREATE TABLE IF NOT EXISTS id_not_null(
+CREATE TABLE IF NOT EXISTS unique_id(
 	id INT default 1 UNIQUE,
 	name VARCHAR(256) not NULL);

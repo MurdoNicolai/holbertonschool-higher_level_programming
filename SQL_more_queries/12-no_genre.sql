0a722c1ec925fc53f756a4cd712522a389c9435d
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-10 03:20:16 +0100

    adding showing shows with null genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- select all shows without a genre linged
+SELECT s.title, sg.genre_id
+	FROM tv_shows s LEFT JOIN tv_show_genres sg ON sg.show_id = s.id
+	WHERE sg.genre_id IS NULL
+	ORDER BY s.title;

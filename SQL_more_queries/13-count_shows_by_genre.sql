1c359fb9d0576fb20a60eace250571ba4b4156f6
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-10 03:43:52 +0100

    retiteling script result

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- dislplays TV Show genre and Number of shows linked to this genre
-SELECT g.name, count(sg.show_id)
+SELECT g.name AS genre, count(sg.show_id) AS number_of_shows
 	FROM tv_genres g
 	RIGHT JOIN tv_show_genres sg ON g.id = sg.genre_id
 	GROUP BY genre_id

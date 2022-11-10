5b2657a90d15dd13ab8bb3d1c0eaf2561558020c
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-10 04:34:28 +0100

    adding selecting all but dexter

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- displays all genres not linked to the show dexter
+SELECT g2.name
+	FROM tv_genres g2
+	LEFT JOIN(
+		SELECT g.name, s.title
+			FROM tv_genres g
+			RIGHT JOIN tv_show_genres sg ON g.id = sg.genre_id
+			RIGHT JOIN tv_shows s ON sg.show_id = s.id
+			WHERE title = 'Dexter'
+		) AS dexterTable
+	ON g2.name = dexterTable.name
+	WHERE title IS NULL

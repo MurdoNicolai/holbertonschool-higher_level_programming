be5e533e2d79781de0e8c7426e14c16866887a8d
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-10 01:59:42 +0100

    changing order or columns in cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -9,8 +9,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 */
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
   id 	  						INTEGER 		    PRIMARY KEY		  AUTO_INCREMENT,
-  name							VARCHAR(256)  		NOT NULL,
   state_id						INTEGER,
+  name							VARCHAR(256)  		NOT NULL,
   FOREIGN KEY (state_id) 		REFERENCES states(id)
 );
 

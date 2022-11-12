3f17b7015f30f92d5dfc00b0be579be3da15c998
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-12 15:46:22 +0100

    pleasing pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -15,23 +15,24 @@ import MySQLdb
 def main():
     """ Does the entire script when not imported"""
 
-    #get information needed
+    """get information needed"""
     Username = sys.argv[1]
     Password = sys.argv[2]
     DBname = sys.argv[3]
     Host = 'localhost'
 
-    #make connection to database
-    database = MySQLdb.connect(host=Host, user=Username, passwd=Password, db=DBname)
+    """make connection to database"""
+    database = MySQLdb.connect(Host, Username, Password, DBname)
 
-    #cursor has simplifed modules
+    """cursor has simplifed modules"""
     cursor = database.cursor()
 
-    #use database to print states
+    """use database to print states"""
     cursor.execute("SELECT * FROM states")
     states = cursor.fetchall()
     for state in states:
-        print (f"({state[0]}, '{state[1]}')")
+        print(f"({state[0]}, '{state[1]}')")
+
 
 if __name__ == '__main__':
     main()

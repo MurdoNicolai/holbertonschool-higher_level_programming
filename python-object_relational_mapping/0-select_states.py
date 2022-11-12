7c36ecd3c7d57e4e1f8ab114c2bf832456e7bdee
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-12 15:38:00 +0100

    adding print states script

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+
+"""lists all states from the database hbtn_0e_0_usa.
+arguments:
+    mysql username (string)
+    mysql password (string)
+    database name (string)
+"""
+def main():
+    
+    #get information needed
+    Username = sys.argv[1]
+    Password = sys.argv[2]
+    DBname = sys.argv[3]
+    Host = 'localhost'
+
+    #make connection to database
+    database = MySQLdb.connect(host=Host, user=Username, passwd=Password, db=DBname)
+
+    #cursor has simplifed modules
+    cursor = database.cursor()
+
+    #use database to print states
+    cursor.execute("SELECT * FROM states")
+    states = cursor.fetchall()
+    for state in states:
+        print (f"({state[0]}, '{state[1]}')")
+
+if __name__ == '__main__':
+    main()

176780d5739eb2eeaaec7c19ec1c962ed406dbda
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-12 17:34:52 +0100

    adding create a table script

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Creates the State class"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Contains class definition of table states:
+
+    id (int): the id of the state
+    name (str): the name of the state
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))

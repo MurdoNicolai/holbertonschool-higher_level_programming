c4e51e547a01f38b74bffb397209c8f9ca678c01
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-12-13 17:42:51 +0100

    adding gett statuscoe frome website

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const webpage = process.argv[2];
+request(webpage, function (error, response) {
+  if (error) {
+    console.error('error:', error);
+  }
+  console.log('statusCode:', response && response.statusCode);
+});

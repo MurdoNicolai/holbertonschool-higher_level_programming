20852da4a30809599c7508942b621ce97783de7e
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-12-14 20:56:50 +0100

    adding find number tasks completed

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const usersTasksCompleted = {};
+
+function NumbercompletedTasks (error, page) {
+  if (error) {
+    console.error('error:', error);
+  }
+  const JSONpage = JSON.stringify(page); // transform to JSON string
+  const JSONbody = JSON.parse(JSON.parse(JSONpage).body); // convert to dict and find body
+  for (let i = 0; i < JSONbody.length; i++) { // go through body to find users
+    if (isNaN(usersTasksCompleted[JSONbody[i].userId])) {
+      usersTasksCompleted[JSONbody[i].userId] = 0;
+    } else if (JSONbody[i].completed) {
+      usersTasksCompleted[JSONbody[i].userId] += 1;
+    }
+  }
+}
+
+const webpage = process.argv[2];
+request(webpage, NumbercompletedTasks);
+
+setTimeout(() => console.log(usersTasksCompleted), 1000);

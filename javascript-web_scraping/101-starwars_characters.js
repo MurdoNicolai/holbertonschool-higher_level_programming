09a58b223e1a1190300d172c41fb723430ca1061
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-12-14 21:52:34 +0100

    same change as 100 done to 101

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -9,7 +9,7 @@ function printCharacterInFilm (error, page) {
   const JSONpage = JSON.stringify(page); // transform to JSON string
   const JSONbody = JSON.parse(JSONpage).body; // convert to dict and find body
   // find caracters of film given as input, convert to list
-  const characters = JSON.parse(JSONbody).results[process.argv[2]].characters;
+  const characters = JSON.parse(JSONbody).results[process.argv[2] - 1].characters;
   for (let characterLink = 0; characterLink < characters.length; characterLink++) {
     request(characters[characterLink], getname); // print character name using link
   }

ca879eebbc85015ac42d524c55cffd2fc8628b98
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-02-15 21:24:47 +0100

    again

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 
-function factorialize(a)
+function factorialize () {
 /* Analysis of string that is done in argument */
-const a = parseInt(process.argv[2]);
+  let a = parseInt(process.argv[2]);
 /* if b is not a number, print 'Missin...' */
-if (a == 0 || a === 1) {
-  console.log('0');
-} else {
+  if (a === 0 || a === 1) {
+    console.log('0');
+  } else {
   for (let i = 0; i < a; i++) {
-	if i
+	  if i
+  }
 }
